d49bc8d6f3b118b5b200cc64a161c7dabd15c9e5
GABRIEL MEIRA <gabriel.meira.2004@gmail.com>
2021-09-06 08:35:12 -0300

    consegue voltar pra linha depois de gap

--- out/final.cs
+++ out/final.cs
@@ -396,13 +396,22 @@ void Setup () {
 	void Centralize (int timeout = 2000) {
 	
 		//If only the external sensors are above the line
-		if (isWhite(new byte[] {1,2,3}) && isBlack(4)) {
-			moveTime(200, 15);
-			while (!isFullBlack(3)) left(1000);
-		} else if (isBlack(1) && isWhite(new byte[] {2,3,4})) {
-			moveTime(200, 15);
-			while (!isFullBlack(2)) right(1000);
-		}
+			checkpoint_centralize:
+			time.reset();
+	
+			if (isWhite(new byte[] {1,2,3}) && isBlack(4)) {
+				moveTime(200, 15);
+				while (!isFullBlack(3) && time.timer() < 500) left(1000);
+			} else if (isBlack(1) && isWhite(new byte[] {2,3,4})) {
+				moveTime(200, 15);
+				while (!isFullBlack(2) && time.timer() < 500) right(1000);
+			}
+	
+			if (time.timer() > 450) {
+				moveTime(300, 30);
+				goto checkpoint_centralize;
+			}
+		//
 	
 		time.reset();
 		do {
