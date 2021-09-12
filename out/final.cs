2664e7661368ca61afc7c7c6a963bbef9890188b
GABRIEL MEIRA <gabriel.meira.2004@gmail.com>
2021-09-11 22:18:21 -0300

    diferencia rampa de gangorra

--- out/final.cs
+++ out/final.cs
@@ -637,27 +637,28 @@ void Setup () {
 	void Ramp () {
 		if (inclination() < -7) {
 			led(color["blue"]);
-			stop();
 	
-			actuator.Up();
-			time.reset();
-			const int timeToRamp = 2000;
+			//lifts the actuator and follows the line for a time then down that
+				stop();
+				actuator.Up();
+				time.reset();
+				while (time.timer() < 2000 + 100*scaleAngle(direction())) {
+					LineFollower();
+				}
+				actuator.Down();
+				int last_inclination = inclination();
+				while (inclination() < -1) LineFollower();
+				int last_inclination2 = inclination();
+			//
 	
-			while (time.timer() < timeToRamp) {
-				LineFollower();
-			}
-			actuator.Down();
-			int last_inclination = inclination();
-	
-			while (inclination() < -5) LineFollower();
-			int last_inclination2 = inclination();
-	
-			if (last_inclination - last_inclination2 > -5) {
-				stop(750);
-				moveTime(-300, 250);
-			} else {
-				stop(200);
-			}
+			//if is a seesaw needs to wait that down
+				if (maths.interval(last_inclination - last_inclination2, -10, 0)) {
+					stop(750);
+					moveTime(-300, 250);
+				} else {
+					stop(200);
+				}
+			//
 		}
 	}
 	void RedEnd () {
