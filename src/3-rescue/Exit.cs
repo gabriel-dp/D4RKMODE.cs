ba9c64d0546a545ef0d95f1e1eaf303dce07590b
GABRIEL MEIRA <gabriel.meira.2004@gmail.com>
2021-09-25 16:02:55 -0300

    exit fix

--- src/3-rescue/Exit.cs
+++ src/3-rescue/Exit.cs
@@ -1,59 +1,61 @@
 void Exit (sbyte side_mod) {
 
-	for (byte i = 0; i < 4; i++) {
-		if (ultra(i) > 360) {
-			while (ultra(i) > 400) left(1000 * side_mod);
+	//verifies if already is in front of a empty space
+		for (byte i = 0; i < 4; i++) {
+			if (ultra(i) > 360) {
+				while (ultra(i) > 400) left(1000 * side_mod);
+			}
 		}
-	}
+	//
 
 	FindTheExitAgain:
 	//goes to the exit
-	led(color["white"]);
+		led(color["white"]);
 
-	//identify which sensor reads more than the limit
-		byte whichSensor = 0;
-		while (whichSensor == 0) {
-			right(1000 * side_mod);
-			for (byte i = 0; i < 4; i++) {
-				if (ultra(i) > 400) {
-					whichSensor = i;
-					break;
+		//identify which sensor reads more than the limit
+			byte whichSensor = 0;
+			while (whichSensor == 0) {
+				right(1000 * side_mod);
+				for (byte i = 0; i < 4; i++) {
+					if (ultra(i) > 400) {
+						whichSensor = i;
+						break;
+					}
 				}
 			}
-		}
-		console(2, $"{whichSensor}");
-	//
+			console(2, $"{whichSensor}");
+		//
 
-	//centralizes with the empty space
-		AlreadyFindedTheExit:
+		//centralizes with the empty space
+			AlreadyFindedTheExit:
 
-		time.reset();
-		while (ultra(whichSensor) > 360) right(1000*side_mod);
-		int timeDetecting = time.timer();
+			time.reset();
+			while (ultra(whichSensor) > 360) right(1000*side_mod);
+			int timeDetecting = time.timer();
 
-		time.reset();
-		while (time.timer() < timeDetecting/1.6) left(1000*side_mod);
+			time.reset();
+			while (time.timer() < timeDetecting/1.6) left(1000*side_mod);
 
-		if (whichSensor == 2) rotate(500, 90);
-		else if (whichSensor == 3) rotate(500, -90);
+			if (whichSensor == 2) rotate(500, 90);
+			else if (whichSensor == 3) rotate(500, -90);
 
-		if (ultra(1) < 400) {
-			goto FindTheExitAgain;
-		}
+			if (ultra(1) < 400) {
+				goto FindTheExitAgain;
+			}
 
-		stop();
-		open_actuator = false;
-		actuator.Down();
-	//
+			stop();
+			open_actuator = false;
+			actuator.Down();
+		//
 
-	//goes to the empty space
-		time.reset();
-		while (isWhite(new byte[] {2,3})) FollowerGyro(direction());
-		int timeToBack = time.timer();
-		moveTime(300, 50);
-	//
+		//goes to the empty space
+			time.reset();
+			while (isWhite(new byte[] {2,3})) FollowerGyro(direction());
+			int timeToBack = time.timer();
+			moveTime(300, 50);
+		//
 
-	//verifies if that is the entrance or the exit
+		//verifies if that is the entrance or the exit
 		if (anySensorColor("blue") && !isThatColor(1, "GREEN") && !isThatColor(2, "GREEN") && !isThatColor(3, "GREEN") && !isThatColor(4, "GREEN")) {
 			led(color["orange"]);
 			moveTime(-300, timeToBack);
