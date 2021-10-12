14f25bde861108094ae13bf65c8ccdeaf0750b5a
GABRIEL MEIRA <gabriel.meira.2004@gmail.com>
2021-10-12 15:19:41 -0300

    teste zerado

--- out/final.cs
+++ out/final.cs
@@ -40,15 +40,7 @@ void Setup () {
 
 void Tests () {
 	if (test) {
-		actuator.Up();
-		if (bot.Heat() > 32) {
-			rotate(500, 30);
-			rotate(500, -60);
-			back(50);
-			actuator.Adjust(22, 0);
-			rotate(500, 30);
-		}
-		stop(9999);
+
 	}
 }
 
--- src/_test.cs
+++ src/_test.cs
@@ -1,13 +1,5 @@
 void Tests () {
 	if (test) {
-		actuator.Up();
-		if (bot.Heat() > 32) {
-			rotate(500, 30);
-			rotate(500, -60);
-			back(50);
-			actuator.Adjust(22, 0);
-			rotate(500, 30);
-		}
-		stop(9999);
+
 	}
 }
